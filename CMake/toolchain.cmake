a13fefac39282c4426ba87c669a375742169e223
Aksel Skauge Mellbye <aksel.mellbye@silabs.com>
2015-09-23 10:57:57 +0200

    Update CMake script to reference CM0+ instead of CM0.

--- CMake/toolchain.cmake
+++ CMake/toolchain.cmake
@@ -11,14 +11,14 @@ set(TARGET_EFM32HG_STK_GCC_TOOLCHAIN_INCLUDED 1)
 # definition that you're about to add to rely on the TARGET_LIKE_XXX
 # definitions that yotta provides based on the target.json file.
 #
-add_definitions("-DEFM32HG322F64 -DEFM32HG -DEFM32 -DARM_MATH_CM0PLUS")
+add_definitions("-DEFM32HG322F64 -DEFM32HG -DEFM32 -DDEBUG_EFM -DARM_MATH_CM0PLUS")
 
 # append non-generic flags, and set EFM32HG-specific link script
 
-set(_CPU_COMPILATION_OPTIONS "-mcpu=cortex-m0 -mthumb -D__thumb2__ -g -g3 -gdwarf-3 -gstrict-dwarf")
+set(_CPU_COMPILATION_OPTIONS "-mcpu=cortex-m0plus -mthumb -D__thumb2__ -g -g3 -gdwarf-3 -gstrict-dwarf")
 
 set(CMAKE_C_FLAGS_INIT             "${CMAKE_C_FLAGS_INIT} ${_CPU_COMPILATION_OPTIONS}")
 set(CMAKE_ASM_FLAGS_INIT           "${CMAKE_ASM_FLAGS_INIT} ${_CPU_COMPILATION_OPTIONS}")
 set(CMAKE_CXX_FLAGS_INIT           "${CMAKE_CXX_FLAGS_INIT} ${_CPU_COMPILATION_OPTIONS}")
-set(CMAKE_MODULE_LINKER_FLAGS_INIT "${CMAKE_MODULE_LINKER_FLAGS_INIT} -mcpu=cortex-m0 -mthumb")
-set(CMAKE_EXE_LINKER_FLAGS_INIT    "${CMAKE_EXE_LINKER_FLAGS_INIT} -mcpu=cortex-m0 -mthumb -T\"${CMAKE_CURRENT_LIST_DIR}/../ld/efm32hg.ld\"")
+set(CMAKE_MODULE_LINKER_FLAGS_INIT "${CMAKE_MODULE_LINKER_FLAGS_INIT} -mcpu=cortex-m0plus -mthumb")
+set(CMAKE_EXE_LINKER_FLAGS_INIT    "${CMAKE_EXE_LINKER_FLAGS_INIT} -mcpu=cortex-m0plus -mthumb -T\"${CMAKE_CURRENT_LIST_DIR}/../ld/efm32hg.ld\"")
